7adb79c71cdfd8d4d4cc4b7b8bfe2c930f8dead7
Abdulloh-Mirzayev <abdullohmirzayev7570@gmail.com>
2023-12-15 00:54:33 +0500

    added new folder place

--- Modern Calendar with Todo/script.js
+++ Modern Calendar with Todo/script.js
@@ -26,7 +26,14 @@ const months = [
 
 // function to add days
 function initCalendar() {
+    // to 
   const firstDay = new Date(year, month, 1);
   const lastDay = new Date(year, month + 1, 0);
-  
+  const prevLastDay = new Date(year, month, 0);
+  const prevDay = prevLastDay.getDate();
+  const ladtDate = lastDay.getDate();
+  const day = firstDay.getDate();
+  const nextDays = 7 - lastDay.getDay() - 1;
+
+  date.innerHTML = `${month[month]} ${year}`;
 }
